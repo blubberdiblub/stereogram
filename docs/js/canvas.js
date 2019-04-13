66f9cc188923ff569efe7b31d539d986e53baad0
Niels Boehm <blubberdiblub@gmail.com>
2019-04-13 22:16:36 +0200

    Split RenderStage off from RenderContext

--- docs/js/canvas.js
+++ docs/js/canvas.js
@@ -435,7 +435,7 @@ window.addEventListener('load', () => {
      */
     const uniformSource = {
         RENDER_OBJECT: 1,
-        RENDER_CONTEXT: 2,
+        RENDER_STAGE: 2,
         SCENE_OBJECT: 3,
         FIRST_FOUND: 0,
 
@@ -445,11 +445,11 @@ window.addEventListener('load', () => {
          * @param {uniformSource} source
          * @param {string} name
          * @param {RenderObject} renderObject
-         * @param {RenderContext} renderContext
+         * @param {RenderStage} renderStage
          *
          * @returns {function():*}
          */
-        getPropertyReader(source, name, renderObject, renderContext) {
+        getPropertyReader(source, name, renderObject, renderStage) {
             let candidates;
 
             switch (source) {
@@ -457,8 +457,8 @@ window.addEventListener('load', () => {
                     candidates = [renderObject];
                     break;
 
-                case uniformSource.RENDER_CONTEXT:
-                    candidates = [renderContext];
+                case uniformSource.RENDER_STAGE:
+                    candidates = [renderStage];
                     break;
 
                 case uniformSource.SCENE_OBJECT:
@@ -466,7 +466,7 @@ window.addEventListener('load', () => {
                     break;
 
                 case uniformSource.FIRST_FOUND:
-                    candidates = [renderObject, renderContext, renderObject.sceneObject];
+                    candidates = [renderObject, renderStage, renderObject.sceneObject];
                     break;
 
                 default:
@@ -501,7 +501,7 @@ window.addEventListener('load', () => {
      * For the user of the class, this is fairly abstract, while the actual
      * implementation can actually consist of any number of function calls.
      *
-     * When a RenderContext selects a command for inclusion, it will prepare
+     * When a RenderStage selects a command for inclusion, it will prepare
      * commands tailored to the RenderObject in that particular context.
      *
      * @private
@@ -516,14 +516,14 @@ window.addEventListener('load', () => {
         }
 
         /**
-         * Prepare concrete commands for an object in a RenderContext
+         * Prepare concrete commands for an object in a RenderStage
          *
          * @param {RenderObject} renderObject
-         * @param {RenderContext} renderContext
+         * @param {RenderStage} renderStage
          *
          * @returns {(function(WebGLRenderingContext):void)[]}
          */
-        prepare(renderObject, renderContext) { // jshint ignore:line
+        prepare(renderObject, renderStage) { // jshint ignore:line
             throw(Error("use a concrete implementation"));
         }
     }
@@ -564,8 +564,8 @@ window.addEventListener('load', () => {
             this.transpose = transpose;
         }
 
-        prepare(renderObject, renderContext) {
-            const reader = uniformSource.getPropertyReader(this.source, this.propertyName, renderObject, renderContext);
+        prepare(renderObject, renderStage) {
+            const reader = uniformSource.getPropertyReader(this.source, this.propertyName, renderObject, renderStage);
 
             const uniform = renderObject.uniforms.get(this.uniformName);
             if (uniform === undefined) {
@@ -573,7 +573,7 @@ window.addEventListener('load', () => {
             }
 
             const {location, type} = uniform;
-            const gl = renderContext.gl;
+            const gl = renderStage.gl;
             const value = reader();
 
             switch (type) {
@@ -743,7 +743,7 @@ window.addEventListener('load', () => {
             }
         }
 
-        prepare(renderObject, renderContext) {
+        prepare(renderObject, renderStage) {
             const elementBuffer = renderObject.elementBuffers.get(this.bufferName);
             if (elementBuffer === undefined) {
                 throw ReferenceError(`missing element buffer ${this.bufferName}`);
@@ -767,7 +767,7 @@ window.addEventListener('load', () => {
             }
 
             const byteOffset = this.offset * byteSize;
-            const mode = drawMode.toGLMode(renderContext.gl, this.mode);
+            const mode = drawMode.toGLMode(renderStage.gl, this.mode);
 
             return [CmdDrawElements._cmd(buffer, mode, count, type, byteOffset)];
         }
@@ -894,12 +894,12 @@ window.addEventListener('load', () => {
         /**
          * @param {SceneObject} sceneObject
          * @param {WebGLRenderingContext} gl
-         * @param {RenderContext} renderContext
+         * @param {RenderStage} renderStage
          * @param {Object} properties
          * @property {string} vertexShaderUrl
          * @property {string} fragmentShaderUrl
          */
-        constructor(sceneObject, gl, renderContext, {
+        constructor(sceneObject, gl, renderStage, {
             inclusionFlags,
             vertexShaderUrl = 'shaders/vertex.vert',
             fragmentShaderUrl = 'shaders/fragment.frag',
@@ -928,7 +928,7 @@ window.addEventListener('load', () => {
                 usedElementBuffers, this.sceneObject.elementArrays);
 
             for (const command of relevantCommands) {
-                this.preparedCommands.push(...command.prepare(this, renderContext));
+                this.preparedCommands.push(...command.prepare(this, renderStage));
             }
         }
 
@@ -1153,7 +1153,7 @@ window.addEventListener('load', () => {
      * Context encapsulating a WebGL context, RenderObjects, some properties
      * and methods for rendering
      */
-    class RenderContext {
+    class RenderStage {
         /**
          * @param {WebGLRenderingContext} gl
          * @param {SceneObject[]} scene
@@ -1177,16 +1177,20 @@ window.addEventListener('load', () => {
             this.gl = gl;
             this.clearColor = clearColor;
             this.view = view;
-            this.aspectX = 1.0;
-            this.aspectY = 1.0;
             this.fieldOfView = fieldOfView;
             this.zNear = zNear;
             this.zFar = zFar;
             this.projection = null;
             this.renderObjects = [];
 
-            this._fiddleCanvas(true);
-            this._calculatePerspective();
+            this._fieldOfView = NaN;
+            this._viewRange = NaN;
+            this._aspectX = NaN;
+            this._aspectY = NaN;
+            this._zNear = NaN;
+            this._zFar = NaN;
+
+            this._initPerspective();
 
             for (const sceneObject of scene) {
                 if (!(sceneObject.inclusionFlags & inclusionFlags)) {
@@ -1201,6 +1205,134 @@ window.addEventListener('load', () => {
         }
 
         /**
+         * Initialize perspective matrix with basic values
+         *
+         * @private
+         *
+         * @returns {void}
+         */
+        _initPerspective() {
+            this._fieldOfView = 90.0;
+            this._viewRange = 1.0;
+            this._aspectX = 1.0;
+            this._aspectY = 1.0;
+            this._zNear = 1.0;
+            this._zFar = Infinity;
+
+            this.projection = new Mat4([
+                1.0, 0.0, 0.0, 0.0,
+                0.0, 1.0, 0.0, 0.0,
+                0.0, 0.0, 1.0, 1.0,
+                0.0, 0.0, -2.0, 0.0,
+            ]);
+        }
+
+        /**
+         * Adjust perspective matrix taking the current field of view and aspect ratio into account
+         *
+         * @private
+         *
+         * @param {number} aspectX
+         * @param {number} aspectY
+         *
+         * @returns {void}
+         */
+        _adjustPerspectiveXY(aspectX, aspectY) {
+            const fieldOfView = this.fieldOfView;
+
+            const changedFieldOfView = fieldOfView !== this._fieldOfView;
+            const changedAspectX = aspectX !== this._aspectX;
+            const changedAspectY = aspectY !== this._aspectY;
+
+            if (changedFieldOfView) {
+                this._fieldOfView = fieldOfView;
+                this._viewRange = Math.tan(Math.PI * 0.5 * (1.0 - fieldOfView / 180.0));
+            }
+
+            if (changedAspectX) { this._aspectX = aspectX; }
+            if (changedAspectY) { this._aspectY = aspectY; }
+
+            if (changedAspectX || changedFieldOfView) { this.projection[0] = this._viewRange * aspectX; }
+            if (changedAspectY || changedFieldOfView) { this.projection[5] = this._viewRange * aspectY; }
+        }
+
+        /**
+         * Adjust perspective matrix taking the current near and far plane into account
+         *
+         * @private
+         *
+         * @returns {void}
+         */
+        _adjustPerspectiveZW() {
+            const zNear = this.zNear;
+            const zFar = this.zFar;
+
+            const changedZNear = zNear !== this._zNear;
+            const changedZFar = zFar !== this._zFar;
+
+            if (changedZNear) { this._zNear = zNear; }
+            if (changedZFar) { this._zFar = zFar; }
+
+            if (changedZFar || changedZNear) {
+                this.projection[10] = (zFar < Infinity) ? (zNear + zFar) / (zFar - zNear) : 1.0;
+                this.projection[14] = (zFar < Infinity) ? -2.0 * zNear * zFar / (zFar - zNear) : -2.0 * zNear;
+            }
+        }
+
+        /**
+         * Render objects with current settings
+         *
+         * @param {number} aspectX
+         * @param {number} aspectY
+         *
+         * @returns {void}
+         */
+        render(aspectX, aspectY) {
+            const gl = this.gl;
+
+            this._adjustPerspectiveXY(aspectX, aspectY);
+            this._adjustPerspectiveZW();
+
+            gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+            gl.clearColor(...this.clearColor);
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            gl.enable(gl.CULL_FACE);
+            gl.enable(gl.DEPTH_TEST);
+
+            for (const obj of this.renderObjects) {
+                gl.useProgram(obj.program);
+
+                for (const command of obj.preparedCommands) {
+                    command(gl);
+                }
+            }
+        }
+    }
+
+    /**
+     * Context encapsulating a WebGL context, RenderObjects, some properties
+     * and methods for rendering
+     */
+    class RenderContext {
+        /**
+         * @param {WebGLRenderingContext} gl
+         * @param {SceneObject[]} scene
+         * @param {Object[]} stages
+         */
+        constructor(gl, scene, stages) {
+            this.gl = gl;
+            this.aspectX = 1.0;
+            this.aspectY = 1.0;
+            this.renderStages = [];
+
+            this._adjustCanvas(true);
+
+            for (const properties of stages) {
+                this.renderStages.push(new RenderStage(gl, scene, properties));
+            }
+        }
+
+        /**
          * Ensure the backing buffer of the canvas has the right size
          *
          * @private
@@ -1209,7 +1341,7 @@ window.addEventListener('load', () => {
          *
          * @returns {boolean}
          */
-        _fiddleCanvas(force=false) {
+        _adjustCanvas(force=false) {
             const canvas = this.gl.canvas;
 
             const width = Math.ceil(canvas.scrollWidth);
@@ -1230,58 +1362,20 @@ window.addEventListener('load', () => {
         }
 
         /**
-         * Calculate perspective matrix taking current aspect ratio into account
-         *
-         * @private
-         *
-         * @returns {void}
-         */
-        _calculatePerspective() {
-            const range = Math.tan(Math.PI * 0.5 * (1.0 - this.fieldOfView / 180.0));
-            const viewRangeX = range * this.aspectX;
-            const viewRangeY = range * this.aspectY;
-
-            const zNear = this.zNear;
-            const zFar = this.zFar;
-
-            const perspectiveZ = (zFar < Infinity) ? (zNear + zFar) / (zFar - zNear) : 1.0;
-            const perspectiveW = (zFar < Infinity) ? -2.0 * zNear * zFar / (zFar - zNear) : -2.0 * zNear;
-
-            this.projection = new Mat4([
-                viewRangeX, 0.0, 0.0, 0.0,
-                0.0, viewRangeY, 0.0, 0.0,
-                0.0, 0.0, perspectiveZ, 1.0,
-                0.0, 0.0, perspectiveW, 0.0,
-            ]);
-        }
-
-        /**
-         * Render objects with current settings
+         * Render stages with current settings
          *
          * @returns {void}
          */
         render() {
             const gl = this.gl;
 
-            if (this._fiddleCanvas()) {
-                this._calculatePerspective();
-            }
+            this._adjustCanvas();
 
             console.assert(gl.drawingBufferWidth === gl.canvas.width);
             console.assert(gl.drawingBufferHeight === gl.canvas.height);
 
-            gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-            gl.clearColor(...this.clearColor);
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-            gl.enable(gl.CULL_FACE);
-            gl.enable(gl.DEPTH_TEST);
-
-            for (const obj of this.renderObjects) {
-                gl.useProgram(obj.program);
-
-                for (const command of obj.preparedCommands) {
-                    command(gl);
-                }
+            for (const stage of this.renderStages) {
+                stage.render(this.aspectX, this.aspectY);
             }
         }
     }
@@ -1316,9 +1410,7 @@ window.addEventListener('load', () => {
                 throw Error("cannot get WebGL context for canvas");
             }
 
-            for (const properties of stages) {
-                renderContexts.push(new RenderContext(gl, scene, properties));
-            }
+            renderContexts.push(new RenderContext(gl, scene, stages));
         }
 
         return renderContexts;
@@ -1376,8 +1468,8 @@ window.addEventListener('load', () => {
             new SceneObject(
                 [
                     new CmdUniformMatrix('u_model_matrix', 'matrix', {source: uniformSource.SCENE_OBJECT}),
-                    new CmdUniformMatrix('u_view_matrix', 'view', {source: uniformSource.RENDER_CONTEXT}),
-                    new CmdUniformMatrix('u_projection_matrix', 'projection', {source: uniformSource.RENDER_CONTEXT}),
+                    new CmdUniformMatrix('u_view_matrix', 'view', {source: uniformSource.RENDER_STAGE}),
+                    new CmdUniformMatrix('u_projection_matrix', 'projection', {source: uniformSource.RENDER_STAGE}),
                     new CmdSetAttrib('a_position', 'main', {size: 3, stride: 6}),
                     new CmdSetAttrib('a_color', 'main', {size: 3, stride: 6, offset: 3}),
                     new CmdDrawElements('main', drawMode.TRIANGLES),
